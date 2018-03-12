8878f4fed979d6fff5a54104e17f24ce0d6a54cf
Samuel Gomis <gomis.samuel@gmail.com>
2018-03-12 13:43:08 +0100

    add login page

---
+++ src/AppBundle/Form/Transformer/ViewTransformer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel
+ * Date: 18-03-12
+ * Time: 1:36 PM
+ */
+
+class ViewTransformer
+{
+
+}
---
+++ src/AppBundle/Model/Contact.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel
+ * Date: 18-03-12
+ * Time: 1:07 PM
+ */
+
+class Contact
+{
+
+}
